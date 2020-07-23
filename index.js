e527fa4a275e890afde59e6a013e16b4d8fa2e51
megancindric <megancindric@gmail.com>
2020-07-23 17:21:23 -0500

    Added button to run ReverseAString function

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+"use strict";
+
 console.log("Hello World");
+
+function askForString() {
+    let response = prompt("What string would you like to reverse?");
+    return response;
+}
+
+function reverseString(stringToReverse){
+var response = "";
+for (var i = stringToReverse.length - 1; i >= 0; i--)
+    {
+    response += stringToReverse[i];
+    }
+return response;
+}
+
+function displayReversedResult(result){
+document.getElementById('revStr').innerHTML = result;    
+}
+
+function reverseStringRoutine(){
+    let stringToReverse = askForString();
+    let output = reverseString(stringToReverse);
+    displayReversedResult(output);
+}
